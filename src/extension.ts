64b6f8af82b1bd919f7e28c7267dc589501233b0
hswfwkj <hswfwkj@163.com>
2023-03-20 17:09:25 +0800

    add a command

--- src/extension.ts
+++ src/extension.ts
@@ -4,10 +4,14 @@ export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "helloWorld" is now active!');
 	
 	let disposable = vscode.commands.registerCommand('my_extension.helloWorld', () => {
-		vscode.window.showInformationMessage('卷王监视器v1.0开发中...作者很垃圾不会TypeScript，在学了在学了');
+		vscode.window.showInformationMessage('卷王监视器v1.0开发中...作者很垃圾不会TypeScript,在学了在学了');
 	});
 	
 	context.subscriptions.push(disposable);
+	let juansee = vscode.commands.registerCommand("my_extension.watch", () => {
+		vscode.window.showInformationMessage("___卷了这些题:_________");
+	});
+	context.subscriptions.push(juansee);
 }
 
 export function deactivate() {}
