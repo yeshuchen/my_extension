d5ee47170d92223c5caf94a2e84b3ad1af1652c9
hswfwkj <hswfwkj@163.com>
2023-03-21 16:21:57 +0800

    a little update

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-
+import * as child_process from 'child_process';
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "helloWorld" is now active!');
 	
@@ -8,9 +8,18 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 	
 	context.subscriptions.push(disposable);
-	let juansee = vscode.commands.registerCommand("my_extension.watch", () => {
-		vscode.window.createWebviewPanel("viewer","卷题列表",vscode.ViewColumn.Two);
-	});
+	let juansee = vscode.commands.registerCommand('my_extension.watch', () => {
+
+		// 创建一个新的窗格
+		const panel = vscode.window.createWebviewPanel(
+		  'watchProblems', // 标识符
+		  '卷题列表', // 标题
+		  vscode.ViewColumn.Two, // 显示位置
+		  {} // 配置选项
+		);
+		// 在窗格中输出Hello!
+		panel.webview.html = `<h1>Hello!</h1>`;
+    });
 	context.subscriptions.push(juansee);
 }
 
